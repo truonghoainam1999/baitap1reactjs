f0be7c75ec2877db77b257b28b41f429464132ce
Hoai Nam <truonghoainam12b1@gmail.com>
2021-04-01 11:57:35 +0700

    edit alt img

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ function Home() {
         <div className="content">
             <h1 className="title">Home page</h1>
             <p className="desc">Start Bootstrap can help you build better websites using the Bootstrap framework! Just download a theme and start customizing, no strings attached!</p>
-            <img className="banner" src={Banner} ></img>
+            <img className="banner" src={Banner} alt="banner" ></img>
         </div>
     );
 }
